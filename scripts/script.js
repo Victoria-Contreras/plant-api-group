0f092612e0c0d4be0ae8fdfdddc62a2979567e94
kngsong1 <kristinagiannoutsos@gmail.com>
2022-11-19 11:40:18 -0600

    script for cards

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,17 @@ async function start () {
 }
 
 start()
+
+function createPeopleList (PeopleList) {
+    document.getElementById("card-container").innerHTML = `
+    <div class="card" style="width: 18rem;" id="quiz-card">
+    <div class="card-body">
+       <h5 class="card-title">Card title</h5>
+       
+       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+       <button href="#" class="card-link">Luke Skywalker</button>
+       <button href="#" class="card-link">Darth Vader</button>
+     </div>
+
+    `
+}
