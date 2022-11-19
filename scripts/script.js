03c47c0af18bc493b4998988fe7e1b0dcf20cdcd
kngsong1 <kristinagiannoutsos@gmail.com>
2022-11-19 11:49:59 -0600

    more edits to script

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 async function start () {
-    const response = await fetch("https://swapi.dev/api/people/")
+    const response = await fetch("https://swapi.dev/api/people")
     const data = await response.json()
   console.log(data)
 }
